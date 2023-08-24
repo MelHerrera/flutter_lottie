e0ab72d7424c9bb29306317af709d507bbf89ec5
MelHerrera <72715151+MelHerrera@users.noreply.github.com>
2023-08-24 16:28:54 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mel/flutter_lottie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mel.flutter_lottie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+//lottie example
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Lottie.asset("assets/animations/llporr82.json"),
+        ),
+      ),
+    );
+  }
+}
+
